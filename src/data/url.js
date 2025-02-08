5864de38dcee9d3b323336bd262e10e233e0b12f
Your Name <s.r.j.s.n2017@gmail.com>
2025-02-08 11:50:01 +0530

    rc

--- src/data/url.js
+++ src/data/url.js
@@ -1,5 +1,5 @@
 //export const baseUrl = "http://localhost:5000/api";
-export const baseUrl = "https://runo-server.onrender.com";
+export const baseUrl = "https://runo-server.onrender.com/api";
 export const apiVersion = "v1";
 //export const baseUrl2 = "http://localhost:5000";
-export const baseUrl2 = "https://runo-server.onrender.com";
+export const baseUrl2 = "https://runo-server.onrender.com/api";
